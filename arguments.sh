639f9626efd89580b5f8a6e60b490aedb2f904a0
Viktor Kolmakov <vitek706@yandex.ru>
2019-06-13 23:29:35 +0300

    3.2 Added arguments

---
+++ arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Name script: $0"
+echo "First parametr is $1."
+echo "Next parametr is $2."
+echo "Second parametr is $3."
+echo "Fist rand is $RANDOM"
+echo "2 rand is $RANDOM"
+echo "3 rand is $RANDOM"
+echo "4 rand is $RANDOM"
+echo "5 rand is $RANDOM"
+chmod -x arguments.sh
